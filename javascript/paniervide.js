c9ae51abf24dafed6ec92c5582e76ae989697e92
sanji92 <abina.habib93@gmail.com>
2020-12-03 17:10:20 +0100

    reboot

--- javascript/paniervide.js
+++
@@ -1,9 +0,0 @@
-// condition de redirection vers la page paniervide.html si localStorage vide
-
-const lienPanier = document.getElementById("lienpanier")
-if (localStorage.length === 0) {
-    lienPanier.setAttribute('href', 'paniervide.html')
-}
-else {
-    location.replace("panier.html")
-}
